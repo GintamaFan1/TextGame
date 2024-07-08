765c6c3c0eba89e95d6c05190a3e3b93c664c69c
GintamaFan1 <165829721+GintamaFan1@users.noreply.github.com>
2024-07-08 19:20:44 +0000

    day 2

--- Puzzle/stage.py
+++ Puzzle/stage.py
@@ -1,14 +1,34 @@
+import rich
+from rich import print
+
+RED = "rgb(255,0,0)"
+BLUE = "rgb(0,0,255)"
+BROWN = "rgb(155,50,155)"
+PURPLE = "rgb(255,0,255)"
 
 class Stage:
-
     def __init__(self):
         self.height = 20
         self.width = 30
         self.tiles = {}
+        for i in range(self.height):
+            for j in range(self.width):
+                self.tiles[(i,j)] = Tiles((i,j))
+    
+    def show_stage(self):
+        for i in range(self.height):
+            for j in range(self.width):
+                tile = self.tiles[(i,j)]
+
+                if tile.exploded == True:
+                    print(f"[{RED}]__ [/{RED}]", end="")
+                else:
+                    print(f"__ ", end="")
+
+
 
 
 class Tiles:
-
     def __init__(self, point):
         self.x = point[0]
         self.y = point[1]
@@ -17,6 +37,7 @@ class Tiles:
         self.broken = False
         self.character_placed_here = False
         self.condition = None
+        self.character = None
         
     
 
